2375f71606d8b3d81e0773e037bea6e5a99b3190
Kory Nelson <kory.nelson99@gmail.com>
2020-10-18 21:28:24 -0700

    added Wokrout an Exercise model as well as index.js to export all models.  This enabled fixing seed.js to seed the mongodb for initial testing

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const seed = require('./seeders/seed.js');
+const Exercise = require("./models/Exercise.js");
 
 const PORT = process.env.PORT || 3000;
 
+// Load models
+//require('./seeders/seed.js')
 const app = express();
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Allow server to use public file
 app.use(express.static("public"));
 
 //add routes
 require("./routes/html-routes.js")(app);
 
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { 
-    useNewUrlParser: true 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+    useNewUrlParser: true ,
+    useUnifiedTopology: true 
 });
+
+    // Exercise.create({type:'deadlift'})
+    //   .then(dbExercise => {
+    //     res.json(dbExercise);
+    //   })
+    //   .catch(err => {
+    //     res.json(err);
+    //   });
+seed();
 
 
 app.listen(PORT, () => {
